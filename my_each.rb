b11f65fa285891ad6348b1245d06c8c08facb347
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-18 21:07:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(collection) # put argument(s) here
   # code here\
-  collection.each do |num|
-    puts num
-  end
+  # collection.each do |num|
+  #   puts num
+  # end
   i = 0
   while i<collection.length
     yield
     
     i+=1
-end
+  end
 end
